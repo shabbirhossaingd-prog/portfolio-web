aa62f4b272cd0d0fced0c113a12c43c76727a7d9
shabbirhossaingd-prog <shabbirhossain.gd@gmail.com>
2026-09-20 02:19:56 +0600

    Prepare embedded hero image assets

---
+++ lib/hero-b64.ts
@@ -0,0 +1,10 @@
+export const heroDarkParts = [
+  // DARK_NEXT
+];
+
+export const heroLightParts = [
+  // LIGHT_NEXT
+];
+
+export const heroDarkBase64 = heroDarkParts.join("");
+export const heroLightBase64 = heroLightParts.join("");
